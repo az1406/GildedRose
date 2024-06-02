ab14a069cfeeefd92a9ba71ab1bc98aba59ae9d3
Aldin <if22b130@technikum-wien.at>
2024-06-02 20:21:43 +0200

    "Aged Brie" actually increases in Quality the older it gets

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -14,4 +14,12 @@ describe("Gilded Rose", function() {
    expect(items[0].quality).toEqual(0);
    expect(items[0].sell_in).toEqual(0);
  });
+
+  it("should increase quality for Aged Brie", function() {
+  items = [ new Item("Aged Brie", 10, 20) ];
+  update_quality();
+  expect(items[0].sell_in).toEqual(9);
+  expect(items[0].quality).toEqual(21);
+ });
+
 });
