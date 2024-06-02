9f64507652172fdbd3c15455cf22c5db0a652253
Aldin <if22b130@technikum-wien.at>
2024-06-02 20:18:07 +0200

    Once the sell by date has passed, Quality degrades twice as fast

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -1,9 +1,11 @@
 describe("Gilded Rose", function() {
 
-  it("should foo", function() {
-    items = [ new Item("foo", 0, 0) ];
+  it("should degrade quality of item twice as fast, after sellIn date passed", function() {
+    items = [ new Item("Elixir of the Mongoose", 0, 10 ) ];
     update_quality();
-    expect(items[0].name).toEqual("fixme");
+    expect(items[0].sell_in).toEqual(-1);
+    expect(items[0].quality).toEqual(8);
+   
   });
 
 });
